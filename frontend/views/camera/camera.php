2253978ef5070ab9f501edeae1b644af99a025e7
Игорь Осока <skit-1@se.local>
2018-12-11 09:04:16 +0300

    yandexMetrika and right sidebar

--- frontend/views/camera/camera.php
+++ frontend/views/camera/camera.php
@@ -21,28 +21,7 @@ $this->registerMetaTag(['name' => 'description', 'content' => $description]);
     </div>
     <br>
     <p><?= $camera['description'] ?></p>
-    <!--
-    <div class="row">
-        <div class="span3">
-            <h5>2 Column Layout</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis mattis lorem, quis gravida nunc
-                iaculis ac. Proin tristique tellus in est vulputate luctus fermentum ipsum molestie. Vivamus tincidunt
-                sem eu magna varius elementum. Maecenas felis tellus, fermentum vitae laoreet vitae, volutpat et urna.
-                Nulla faucibus ligula eget ante varius ac euismod odio placerat. Nam sit amet felis non lorem faucibus
-                rhoncus vitae id dui.</p>
-            <button class="btn btn-mini btn-inverse" type="button">Read more</button>
-        </div>
-        <div class="span3">
-            <h5>2 Column Layout</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis mattis lorem, quis gravida nunc
-                iaculis ac. Proin tristique tellus in est vulputate luctus fermentum ipsum molestie. Vivamus tincidunt
-                sem eu magna varius elementum. Maecenas felis tellus, fermentum vitae laoreet vitae, volutpat et urna.
-                Nulla faucibus ligula eget ante varius ac euismod odio placerat. Nam sit amet felis non lorem faucibus
-                rhoncus vitae id dui.</p>
-            <button class="btn btn-mini btn-inverse" type="button">Read more</button>
-        </div>
-    </div>
-    -->
+
 </div> <!--End page content column-->
 
 <!-- Page Right Sidebar
@@ -50,67 +29,23 @@ $this->registerMetaTag(['name' => 'description', 'content' => $description]);
 <div class="span3 sidebar page-right-sidebar"><!-- Begin sidebar column -->
 
     <!--User Login-->
-    <h5 class="title-bg">User Login</h5>
-    <div class="input-prepend">
-        <span class="add-on"><i class="icon-user"></i></span><input class="span2" id="prependedInput" size="16"
-                                                                    type="text" placeholder="Username">
-    </div>
-    <div class="input-prepend">
-        <span class="add-on"><i class="icon-lock"></i></span><input class="span2" id="appendedPrependedInput" size="16"
-                                                                    type="text" placeholder="Password">
-    </div>
-    <button class="btn btn-small btn-inverse" type="button">Login</button>
+    <h5 class="title-bg">Немного о проекте</h5>
+            <div class="tab-pane" id="about">
+            <p>Наш каталог онлайн камер позволит Вам увидеть новые места. 
+			На нашем сайте собраны камеры, транслирующие видео в реальном времени. 
+			Мы, совершенно бесплатно, предлагаем посетить интересные уголки нашей планеты, не вставая с дивана, 
+			увидеть уникальные строения, познать жизнь редких животных, узнать, 
+			как живут другие люди в отдалённых уголках планеты, 
+			очутиться на пляже океана и все это в режиме реального времени, не выходя из дома. 
+			</p>
+
+            Мы постоянно пополняем нашу коллекцию трансляций, оставайтесь с нами, и вы откроете для себя много нового.
+        </div>
 
     <!--Tabbed Content-->
-    <h5 class="title-bg">More Info</h5>
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#comments" data-toggle="tab">Comments</a></li>
-        <li><a href="#tweets" data-toggle="tab">Tweets</a></li>
-        <li><a href="#about" data-toggle="tab">About</a></li>
-    </ul>
+    
 
-    <div class="tab-content">
-        <div class="tab-pane active" id="comments">
-            <ul>
-                <li><i class="icon-comment"></i>admin on <a href="#">Lorem ipsum dolor sit amet</a></li>
-                <li><i class="icon-comment"></i>admin on <a href="#">Consectetur adipiscing elit</a></li>
-                <li><i class="icon-comment"></i>admin on <a href="#">Ipsum dolor sit amet consectetur</a></li>
-                <li><i class="icon-comment"></i>admin on <a href="#">Aadipiscing elit varius elementum</a></li>
-                <li><i class="icon-comment"></i>admin on <a href="#">ulla iaculis mattis lorem</a></li>
-            </ul>
-        </div>
-        <div class="tab-pane" id="tweets">
-            <ul>
-                <li><a href="#"><i class="icon-share-alt"></i>@room122</a> Vivamus tincidunt sem eu magna varius
-                    elementum. Maecenas felis tellus, fermentum vitae laoreet vitae, volutpat et urna.
-                </li>
-                <li><a href="#"> <i class="icon-share-alt"></i>@room122</a> Nulla faucibus ligula eget ante varius ac
-                    euismod odio placerat.
-                </li>
-                <li><a href="#"> <i class="icon-share-alt"></i>@room122</a> Pellentesque iaculis lacinia leo. Donec
-                    suscipit, lectus et hendrerit posuere, dui nisi porta risus, eget adipiscing
-                </li>
-                <li><a href="#"> <i class="icon-share-alt"></i>@room122</a> Vivamus augue nulla, vestibulum ac ultrices
-                    posuere, vehicula ac arcu.
-                </li>
-                <li><a href="#"> <i class="icon-share-alt"></i>@room122</a> Sed ac neque nec leo condimentum rhoncus.
-                    Nunc dapibus odio et lacus.
-                </li>
-            </ul>
-        </div>
-        <div class="tab-pane" id="about">
-            <p>Enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
-                skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt
-                aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
-                helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
-                vice lomo.</p>
-
-            Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda
-            shoreditch et.
-        </div>
-    </div>
-
-    <!--Progress Bars-->
+    <!--Progress Bars
     <h5 class="title-bg">Progress Bars</h5>
     <div class="progress progress-info progress-striped">
         <div class="bar" style="width: 20%"></div>
@@ -124,7 +59,7 @@ $this->registerMetaTag(['name' => 'description', 'content' => $description]);
     <div class="progress progress-danger progress-striped">
         <div class="bar" style="width: 80%"></div>
     </div>
-
+-->
 </div><!-- End sidebar column -->
 
 
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -23,6 +23,20 @@ AppAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
     <?php $this->head() ?>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ym(51511571, "init", {
+id:51511571,
+clickmap:true,
+trackLinks:true,
+accurateTrackBounce:true
+});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/51511571" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 </head>
 <body class="home">
@@ -49,7 +63,7 @@ AppAsset::register($this);
 			
                 <ul class="nav">
 					<li class="active"><a href="/" >Гланая</a></li>
-					<li><a href="list">Камеры</a></li>
+					<li><a href="list">Онлайн веб камеры</a></li>
                     <!--
 					<li class="dropdown active">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="index.htm">Home <b
@@ -60,42 +74,6 @@ AppAsset::register($this);
                             <li><a href="index-slider.htm">Slider Only</a></li>
                         </ul>
                     </li>
-
-
-                    <li><a href="features.htm">Features</a></li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="page-full-width.htm">Pages <b
-                                    class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="page-full-width.htm">Full Width</a></li>
-                            <li><a href="page-right-sidebar.htm">Right Sidebar</a></li>
-                            <li><a href="page-left-sidebar.htm">Left Sidebar</a></li>
-                            <li><a href="page-double-sidebar.htm">Double Sidebar</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="gallery-4col.htm">Gallery <b
-                                    class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="gallery-3col.htm">Gallery 3 Column</a></li>
-                            <li><a href="gallery-4col.htm">Gallery 4 Column</a></li>
-                            <li><a href="gallery-6col.htm">Gallery 6 Column</a></li>
-                            <li><a href="gallery-4col-circle.htm">Gallery 4 Round</a></li>
-                            <li><a href="gallery-single.htm">Gallery Single</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="blog-style1.htm">Blog <b
-                                    class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="blog-style1.htm">Blog Style 1</a></li>
-                            <li><a href="blog-style2.htm">Blog Style 2</a></li>
-                            <li><a href="blog-style3.htm">Blog Style 3</a></li>
-                            <li><a href="blog-style4.htm">Blog Style 4</a></li>
-                            <li><a href="blog-single.htm">Blog Single</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="page-contact.htm">Contact</a></li>
 					-->
                 </ul>
 
@@ -113,24 +91,6 @@ AppAsset::register($this);
 						<option value="index-gallery.htm">- Gallery Only</option>
                         <option value="index-slider.htm">- Slider Only</option>
                         <option value="features.htm">Features</option>
-                        <option value="page-full-width.htm">Pages</option>
-                        <option value="page-full-width.htm">- Full Width</option>
-                        <option value="page-right-sidebar.htm">- Right Sidebar</option>
-                        <option value="page-left-sidebar.htm">- Left Sidebar</option>
-                        <option value="page-double-sidebar.htm">- Double Sidebar</option>
-                        <option value="gallery-4col.htm">Gallery</option>
-                        <option value="gallery-3col.htm">- 3 Column</option>
-                        <option value="gallery-4col.htm">- 4 Column</option>
-                        <option value="gallery-6col.htm">- 6 Column</option>
-                        <option value="gallery-4col-circle.htm">- Gallery 4 Col Round</option>
-                        <option value="gallery-single.htm">- Gallery Single</option>
-                        <option value="blog-style1.htm">Blog</option>
-                        <option value="blog-style1.htm">- Blog Style 1</option>
-                        <option value="blog-style2.htm">- Blog Style 2</option>
-                        <option value="blog-style3.htm">- Blog Style 3</option>
-                        <option value="blog-style4.htm">- Blog Style 4</option>
-                        <option value="blog-single.htm">- Blog Single</option>
-                        <option value="page-contact.htm">Contact</option>
 						-->
                     </select>
                 </div>
@@ -147,32 +107,7 @@ AppAsset::register($this);
         <?=\frontend\widgets\randomcameras\RandomCamerasWidget::widget()?>
 
 
-        <!--Latest News
-        <h5 class="title-bg">Latest News</h5>
-        <ul class="popular-posts">
-            <li>
-                <a href="#"><img src="img/gallery/gallery-img-2-thumb.jpg" alt="Popular Post"></a>
-                <h6><a href="#">Lorem ipsum dolor sit amet consectetur adipiscing elit</a></h6>
-                <em>Posted on 09/01/15</em>
-            </li>
-            <li>
-                <a href="#"><img src="img/gallery/gallery-img-2-thumb.jpg" alt="Popular Post"></a>
-                <h6><a href="#">Nulla iaculis mattis lorem, quis gravida nunc iaculis</a></h6>
-                <em>Posted on 09/01/15</em>
-            <li>
-                <a href="#"><img src="img/gallery/gallery-img-2-thumb.jpg" alt="Popular Post"></a>
-                <h6><a href="#">Vivamus tincidunt sem eu magna varius elementum</a></h6>
-                <em>Posted on 09/01/15</em>
-            </li>
-        </ul>
--->
-        <!--Testimonials
-        <h5 class="title-bg">Testimonials</h5>
-
-        <p class="quote-text side">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum felis fermentum."<cite>- Client Name, Big Company</cite></p>
-
-        <p class="quote-text side">"Adipiscing elit. In interdum felis fermentum ipsum molestie sed porttitor ligula rutrum."<cite>- Another Client Name, Big Company</cite></p>
--->
+       
 
     </div><!-- End sidebar column -->
         <?= $content ?>
